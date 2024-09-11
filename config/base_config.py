18535d84be799c5042eef249397dc5e1e0fab76e
yogjun <280536928@qq.com>
2024-09-11 19:21:25 +0800

    关闭爬二级评论

--- config/base_config.py
+++ config/base_config.py
@@ -50,7 +50,7 @@ ENABLE_GET_COMMENTS = True
 
 # 是否开启爬二级评论模式, 默认不开启爬二级评论
 # 老版本项目使用了 db, 则需参考 schema/tables.sql line 287 增加表字段
-ENABLE_GET_SUB_COMMENTS = True
+ENABLE_GET_SUB_COMMENTS = False
 
 # 指定小红书需要爬虫的笔记ID列表
 XHS_SPECIFIED_ID_LIST = [
