87a6ef9a9fea7e9be03b2684f7455936cc83ae19
yogjun <280536928@qq.com>
2024-11-06 14:22:26 +0800

    commit dev

--- config/db_config.py
+++ config/db_config.py
@@ -14,7 +14,7 @@ import os
 # mysql config
 RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "123456")
 RELATION_DB_USER = os.getenv("RELATION_DB_USER", "root")
-RELATION_DB_HOST = os.getenv("RELATION_DB_HOST", "localhost")
+RELATION_DB_HOST = os.getenv("RELATION_DB_HOST", "192.168.34.231")
 RELATION_DB_PORT = os.getenv("RELATION_DB_PORT", "3306")
 RELATION_DB_NAME = os.getenv("RELATION_DB_NAME", "media_crawler")
 
